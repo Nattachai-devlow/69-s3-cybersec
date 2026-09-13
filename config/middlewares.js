32526f93e9c669daf5481fed0c631a73e43b1cfc
Nattachai-devlow <nattachai0046@gmail.com>
2026-09-14 01:05:01 +0700

    fix: enforce register disable at bootstrap + lock CORS to known origins
    - src/index.js bootstrap: flip allow_register=false on every boot so
      fresh clones deploy with open registration blocked by default
    - config/middlewares.js: CORS whitelist (localhost:3000/8080/5173 only)
    - docker-compose.yaml: add src/index.js bind mount

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,7 +6,24 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      // อนุญาตเฉพาะ origin ที่รู้จัก (เพิ่ม origin ของ front-end เองด้วย)
+      origin: [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      credentials: true,
+      keepHeadersOnError: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
